8b8f34194ba66b317fcf1bbcb192b940d4e96034
Abdi Tefera <abditefera11@gmail.com>
2024-03-20 09:51:58 +0300

    some change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className="py-10 text-white bg-[#232325] h-auto" id="about">
       <div className="flex sm:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
         <div>
-          <div className="w-[400px] h-full">
+          <div className="w-full sm:w-[400px] h-full">
             <img
               src={aboutImg}
               alt="about picture"
